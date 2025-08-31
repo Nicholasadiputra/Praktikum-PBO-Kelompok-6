57b578442d7988d050ac6a6c13420a93c7eaf07b
Muhammad Rizqy Putra Pratama <gtau4534@gmail.com>
2025-08-31 11:11:02 +0700

    Mengganti folder lama dengan folder baru

---
+++ project2N/src/main/java/com/mycompany/project2n/Project2N.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project2n;
+
+/**
+ *
+ * @author HP
+ */
+public class Project2N {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
