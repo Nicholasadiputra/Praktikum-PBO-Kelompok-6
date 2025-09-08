0b49ce9641c2a798780025c730bdf888cc2ae2db
Nicholas Adiputra <nicholasa2di@gmail.com>
2025-09-08 18:49:39 +0700

    Add files via upload

---
+++ HasilPraktikum3/OperatorPerbandingan/src/main/java/com/mycompany/operatorperbandingan/OperatorPerbandingan.java
@@ -0,0 +1,15 @@
+package com.mycompany.operatorperbandingan;
+
+public class OperatorPerbandingan {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 8;
+
+        System.out.println("x == y: " + (x == y)); // x sama dengan y, false
+        System.out.println("x != y: " + (x != y)); // x tidak sama dengan y, true 
+        System.out.println("x > y: " + (x > y));  // x lebih besar dari y, false
+        System.out.println("x < y: " + (x < y));  // x lebih kecil dari y, true
+        System.out.println("x >= y: " + (x >= y)); // x lebih besar sama dengan y, false
+        System.out.println("x <= y: " + (x <= y)); // x lebih kecil sama dengan y, true
+    }
+}
