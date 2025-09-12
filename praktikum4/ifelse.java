999f3386c2aa5b3e911d8e3bd1e99c65f02445ca
Nicholas Adiputra <nicholasa2di@gmail.com>
2025-09-12 21:26:05 +0700

    Update ifelse.java

--- praktikum4/ifelse.java
+++ praktikum4/ifelse.java
@@ -1,12 +1,17 @@
+// Import Scanner class untuk membaca input dari user
 import java.util.Scanner;
 
+// Deklarasi class ifelse
 public class ifelse {
     public static void main(String[] args)
     {
+        // Membuat objek Scanner untuk menerima input
         Scanner input =  new Scanner(System.in);
         System.out.print("Masukan nilai : ");
+        // Menyimpan input nilai ke dalam variabel 'nilai'
         int nilai = input.nextInt();
 
+        // Mengecek kondisi nilai
         if (nilai>75){
             System.out.println("Anda lulus ujian");
         } else {
