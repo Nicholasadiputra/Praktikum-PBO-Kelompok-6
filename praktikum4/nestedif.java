64b74c318aab9f898b947c6d94503ee31820e5df
Nicholas Adiputra <nicholasa2di@gmail.com>
2025-09-12 21:35:56 +0700

    Update nestedif.java

--- praktikum4/nestedif.java
+++ praktikum4/nestedif.java
@@ -1,24 +1,34 @@
+// Import Scanner untuk input
 import java.util.Scanner;
 
 public class nestedif {
     
     public static void main(String[] args) {
+        // Membuat objek Scanner
         Scanner input = new Scanner(System.in);
 
+        // Input angka dari user
         System.out.print("Masukkan angka : ");
         int angka = input.nextInt();
 
+        // Cek angka nol
         if (angka == 0) {
             System.out.println("Merupakan bilangan bernilai netral");
-        } else if (angka > 0) {
+        } 
+        // Cek angka positif
+        else if (angka > 0) {
             System.out.print("Merupakan bilangan positif ");
+            // Cek genap/ganjil
             if (angka % 2 == 0) {
                 System.out.println("dan genap");
             }else{
                 System.out.println("dan ganjil");
             }
-        } else {
+        } 
+        // Cek angka negatif
+        else {
             System.out.print("Bilangan bernilai negatif ");
+            // Cek genap/ganjil
             if (angka % 2 == 0){
                 System.out.println("dan genap");
             } else {
