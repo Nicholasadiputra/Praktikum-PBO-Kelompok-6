629dc2e40590b1d608c519fa0a944c12a18b9a90
Nicholas Adiputra <nicholasa2di@gmail.com>
2025-09-12 21:33:49 +0700

    Update loopfor.java

--- praktikum4/loopfor.java
+++ praktikum4/loopfor.java
@@ -1,14 +1,19 @@
+// Import Scanner untuk input
 import java.util.Scanner;
 
+// Class utama
 public class loopfor {
     public static void main(String[] args)
     {
+        // Perulangan for dari 1 sampai 5
         for (int i = 1; i <= 5; i++) {
             System.out.println("Perulangan ke-" + i);
         }
 
+        // Array nama mahasiswa
         String[] mahasiswa = {"Andi", "Budi", "Caca", "Dewi", "Eko"};
 
+        // Perulangan untuk menampilkan nama mahasiswa
         for (int i = 0; i < mahasiswa.length; i++) {
             System.out.println("Mahasiswa ke-" + (i + 1) + " : " + mahasiswa[i]);
         }
