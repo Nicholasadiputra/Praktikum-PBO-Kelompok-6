6e78d81cb0b89b4a0562464dde33ce248f98a00d
Nicholas Adiputra <nicholasa2di@gmail.com>
2025-09-12 21:36:33 +0700

    Update nestedloop.java

--- praktikum4/nestedloop.java
+++ praktikum4/nestedloop.java
@@ -1,13 +1,19 @@
 public class nestedloop {
     public static void main(String[] args)
     {   
+        // Judul program
         System.out.println("---Mencetak nomor kursi bioskop---");
+        // Inisialisasi variabel kursi dengan huruf 'A'
         char kursi = 'A';
+        // Loop baris (A,B,C)
         for (int i = 0; i < 3; i++) {
+            // Loop kolom (1,2,3)
             for (int j = 1; j <= 3; j++) {
                 System.out.println(kursi + " " + j + " ");
             }
+            // Baris baru setiap ganti huruf
             System.out.println();
+            // Increment huruf kursi
             kursi++;
         }
     }
