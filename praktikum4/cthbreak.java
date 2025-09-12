4a63234017d4e2d7052b384e73d0ad42c96e7782
Nicholas Adiputra <nicholasa2di@gmail.com>
2025-09-12 21:24:06 +0700

    Update cthbreak.java

--- praktikum4/cthbreak.java
+++ praktikum4/cthbreak.java
@@ -1,11 +1,14 @@
 public class cthBreak {
     public static void main(String[] args)
     {
+        // Loop dari 1 sampai 10
         for (int d = 1; d <= 10; d++) {
+            // Cek jika d sama dengan 5
             if (d == 5) {
                 System.out.println("Break di angka : " + d);
                 break; // Keluar dari loop ketika d bernilai 5
             }
+            // Cetak angka selain 5
             System.out.println("Angka : " + d);
         }
     }
