c04a545b90721774f53796c0529ab70f76da6e77
Nicholas Adiputra <nicholasa2di@gmail.com>
2025-09-12 21:37:13 +0700

    Update switchcase.java

--- praktikum4/switchcase.java
+++ praktikum4/switchcase.java
@@ -1,12 +1,16 @@
+// Import Scanner untuk input
 import java.util.Scanner;
 
 public class switchcase {
     public static void main(String[] args)
     {
+        // Membuat objek Scanner
         Scanner input =  new Scanner(System.in);
+        // Input warna dari user
         System.out.print("Masukan warna (RGB) : ");
         String warna = input.nextLine();
         
+        // Switch case untuk pemilihan warna
         switch (warna) { 
             case "R":
             System.out.println("Anda memilih warna Merah");
