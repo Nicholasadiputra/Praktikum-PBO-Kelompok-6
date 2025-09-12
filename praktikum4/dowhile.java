f9cb12bca26ff1af133451f03f979bf16b281a82
Nicholas Adiputra <nicholasa2di@gmail.com>
2025-09-12 21:25:01 +0700

    Update dowhile.java

--- praktikum4/dowhile.java
+++ praktikum4/dowhile.java
@@ -1,10 +1,13 @@
+// Class utama
 public class dowhile {
     public static void main(String[] args)
     {
+        // Inisialisasi variabel x
         int x = 6;
+        // Loop do-while, dieksekusi minimal 1x meskipun kondisi false
         do {
             System.out.println("Do While Loop ke-" + x);
             x++;
-        } while (x <= 5);
+        } while (x <= 5); // Cek kondisi setelah eksekusi
     }
 }
