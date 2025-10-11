9aa62f2833b02f5e9c3af8c544ba7ed4439ee50b
Nicholas Adiputra <nicholasa2di@gmail.com>
2025-10-11 16:57:30 +0700

    Create Rekening.java

---
+++ praktikum6/Rekening.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.main;
+
+/**
+ *
+ * @author user
+ */
+public class Rekening {
+    private int nomorRekening;
+    private String namaNasabah;
+    private double saldo;
+
+    public void setNomorRekening(int nomorRekening) {
+        this.nomorRekening = nomorRekening;
+    }
+
+    public void setNamaNasabah(String namaNasabah) {
+        this.namaNasabah = namaNasabah;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getNomorRekening() {
+        return nomorRekening;
+    }
+
+    public String getNamaNasabah() {
+        return namaNasabah;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
+
+
+
