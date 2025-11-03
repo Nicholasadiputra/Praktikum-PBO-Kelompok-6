4858c69c23df4b2bcc449ddb251dff9c8129dd14
Nicholas Adiputra <nicholasa2di@gmail.com>
2025-11-03 20:17:29 +0700

    Add files via upload

---
+++ praktikum9/praktikum9/latihanPrakikum9/src/main/java/com/mycompany/latihanprakikum9/LatihanPrakikum9.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihanprakikum9;
+
+/**
+ *
+ * @author user
+ */
+public class LatihanPrakikum9 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
