0ea3e073cbda596365bc37df704c99d072408737
fahryyakbar <fahryakbar1110@gmail.com>
2025-11-04 07:19:34 +0700

    Add files via upload

---
+++ Praktikum9/Mahasiswa.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.latihanpbo9;
+import java.util.ArrayList;
+/**
+ *
+ * @author user
+ */
+public class Mahasiswa {
+    private String nama;
+    private String nim;
+    private String prodi;
+    private String jenisKelamin;
+    private boolean isActive;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public boolean isActive() {
+        return this.isActive;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public Mahasiswa(String nama, String nim, String prodi, String jenisKelamin, boolean isActive) {
+        this.nama = nama;
+        this.nim = nim;
+        this.prodi = prodi;
+        this.jenisKelamin = jenisKelamin;
+        this.isActive = isActive;
+    }
+
+    public static ArrayList<Mahasiswa> mahasiswas = new ArrayList<>();
+}
